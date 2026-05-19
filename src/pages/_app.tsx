ed6f3b6259cd9361f93c1c55a1fa98aceac18449
woo <khw19980926@gmail.com>
2026-05-19 16:47:44 +0900

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const onClick = ()=>{
+    router.push('/test');
+  }
+  return (
+    <>
+        <header>
+          <Link href="/">Home</Link>
+          <Link href="/search">Search</Link>
+          <Link href="/book/1">Book/1</Link>
+          <div>
+            <button onClick={onClick}>/test로 이동</button>
+          </div>
+        </header>
+        <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+export default function Page(){
+    const router = useRouter();
+    const {name} = router.query;
+    return (
+        <div>Search {name}</div>
+    )
+}
