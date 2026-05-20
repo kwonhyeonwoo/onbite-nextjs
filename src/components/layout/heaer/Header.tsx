af034a350473ac6d97641b95469ca6529c5e3992
woo <khw19980926@gmail.com>
2026-05-20 14:15:36 +0900

    feature: global-layout 스타일링

---
+++ src/components/layout/heaer/Header.tsx
@@ -0,0 +1,8 @@
+import styles from "./index.module.css";
+export default function Header(){
+    return (
+        <header className={styles.header}>
+            <h1>ONEBITE BOOKS</h1>
+        </header>
+    )
+}
